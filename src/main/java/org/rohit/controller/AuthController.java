d66df22e28d066c67cd9e75b774c0ef2f9a5231b
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-09-01 15:17:43 +0530

    controller for authentication api

---
+++ src/main/java/org/rohit/controller/AuthController.java
@@ -0,0 +1,70 @@
+package org.rohit.controller;
+
+import org.rohit.dto.JwtRequest;
+import org.rohit.dto.JwtResponse;
+import org.rohit.security.JwtHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api")
+public class AuthController {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+
+    @Autowired
+    private JwtHelper helper;
+
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
+    @PostMapping("/auth")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request) {
+        System.out.println("controller auth");
+        this.doAuthenticate(request.getUsername(), request.getPassword());
+
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        String token = this.helper.generateToken(userDetails);
+
+        logger.info(request.getUsername());
+        logger.info(request.getPassword());
+
+        JwtResponse response = JwtResponse.builder()
+                .access_token(token)
+                .expires_in(1800)
+                .refresh_expires_in(1800)
+                .refresh_token("")
+                .token_type("Bearer")
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private void doAuthenticate(String email, String password) {
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, password);
+        try {
+            manager.authenticate(authentication);
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException(" Invalid Username or Password  !!");
+        }
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public String exceptionHandler() {
+        return "Credentials Invalid !!";
+    }
+}
