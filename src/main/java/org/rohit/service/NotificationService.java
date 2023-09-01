579ea196c7d32c71d164a110c38ac6bfcd1b9d19
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-09-01 15:23:51 +0530

    implemented logic for validate token

--- src/main/java/org/rohit/service/NotificationService.java
+++ src/main/java/org/rohit/service/NotificationService.java
@@ -4,6 +4,8 @@ import org.rohit.model.NotificationLog;
 import org.rohit.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,25 +19,31 @@ public class NotificationService {
     @Value("${notification.waitTime}")
     private int notificationWaitTime;
 
-    public boolean validateToken() {
-        // validate token: ToDo
+    public boolean validateToken(String username) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() != null) {
+            String principalUsername = authentication.getName();
+            return principalUsername.equals(username);
+        }
         return false;
     }
 
     public void sendEmail(String email, String message) {
 
-        LocalDateTime currentTime= LocalDateTime.now();
-        NotificationLog notificationLog= notificationRepository.findFirstByEmailOrderBySentAtDesc(email);
+        LocalDateTime currentTime = LocalDateTime.now();
+        NotificationLog logToBeSaved = new NotificationLog(UUID.randomUUID(), email, currentTime);
+        NotificationLog notificationLog = notificationRepository.findFirstByEmailOrderBySentAtDesc(email);
 
-        if(currentTime.getMinute()-notificationLog.getSentAt().getMinute() < notificationWaitTime){
-            System.out.println("You can't send the email from "+email+", please use another mail or try later");
-        }
-        else{
-            System.out.println("Email sent successfully! "+message);
+        if (notificationLog == null) {
+            System.out.println("Email sent successfully! " + message);
 
-            NotificationLog logToBeSaved= new NotificationLog(
-                    UUID.randomUUID(),email,currentTime
-            );
+            notificationRepository.save(logToBeSaved);
+        } else if (currentTime.getMinute() - notificationLog.getSentAt().getMinute() < notificationWaitTime) {
+            System.out.println("You can't send the email from " + email + ", please use another mail or try later");
+        } else {
+            System.out.println("Email sent successfully! " + message);
+
             notificationRepository.save(logToBeSaved);
         }
     }
